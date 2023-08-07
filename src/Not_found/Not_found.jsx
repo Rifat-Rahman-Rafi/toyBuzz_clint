bee1b412e5d1080c68f8bff99dc97d98a1a10e8e
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-08-07 16:09:45 +0600

    404 page edit

--- src/Not_found/Not_found.jsx
+++ src/Not_found/Not_found.jsx
@@ -10,8 +10,8 @@ const Not_found = () => {
             <h3 className='mb-8 font-extrabold text-9xl text-gray-600'>
             <span className='sr-only'>Error</span> {status || 404}
           </h3>
-     
-               <Link to='/'><button className='bg-[#FFB01F] font-bold text-white p-3 rounded-md'>GOT TO HOME PAGE</button></Link>
+          
+               <Link to='/'><button className='bg-[#36D399] font-bold text-white p-3 rounded-md'>GOT TO HOME PAGE</button></Link>
             </div>
         </div>
         
